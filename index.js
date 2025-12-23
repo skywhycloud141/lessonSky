4fe3f58662dc5a446bd347ee8fd9e65ca861710a
Sky <daniilanufriev0@gmail.com>
2025-12-23 17:17:31 +0500

    Lesson

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-console.log("Hello World");
-console.log("Help")
-y = 14 
+//git config --global user.email "test@gmai.com"
+//git config --global user.name "testName"
+// 1.git init
+// 2.git add .
+// 3. git commit -m "Init"
+// 4. git push
+
+const files = ["cat.jpg","Virus.exe","dog.jpg","document.pdf",];
+
+for (const file of files){
+    if (file==="cat.jpg"){
+        console.log("Данную картинку пропускаем");
+        continue;
+    }
+
+
+    if (file === "Virus.exe"){
+        console.log("Обнаружен вирус");
+        break;
+    }
+    console.log("Файл проверен")
+}
+ 
+for (const file of files){
+console.log(file)
+}
+let fuel = 100;
+while (fuel>0){
+    console.log("Едем "+"Топлива осталось: "+ fuel);
+    fuel-=10; //fuel = fuel-10
+}
+console.log("Приехали")
+// for (Старт ,Финиш,Шаг)
+
+for (let i = 1; i<= 5;i++){
+
+console.log("Отжимания номер "+i)
+}
+
+
+
